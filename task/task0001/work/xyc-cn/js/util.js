1dcc8aec941759ebb739ad965dd5d01bbd5ea76f
xyc <278255596@qq.com>
2015-04-18 09:34:45 +0800

    开始

---
+++ task/task0001/work/xyc-cn/js/util.js
@@ -0,0 +1,115 @@
+/**
+ * Created by xieyicheng on 2015/4/17.
+ */
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr){
+    return arr instanceof(Array);
+}
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    return typeof fn == 'function';
+}
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+    var ret = src;
+    if(isArray(src)){
+        ret=[];
+        for(var i= 0,length = src.length;i<length;i++){
+            if(typeof src[i] === 'object'){
+                ret[i]= cloneObject(src[i]);
+            }
+            else{
+                ret[i]=src[i];
+            }
+        }
+        return ret;
+    }
+    if(src instanceof Object){
+        ret = {};
+        for(i in src) {
+            if(typeof src[i] === 'object'){
+                ret[i]= cloneObject(src[i]);
+            }
+            else{
+                ret[i]=src[i];
+            }
+        }
+        return ret;
+    }
+
+    return ret;
+}
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(arr) {
+   if(isArray(arr)){
+       var ret = [];
+       for(var i= 0,j = 0,length = arr.length;i<length;i++){
+           if(ret.indexOf(arr[i])==-1){
+               ret[j++] = arr[i];
+           }
+       }
+       return ret;
+   }
+    else{
+       throw new TypeError();
+   }
+}
+// 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
+// 先暂时不要简单的用一句正则表达式来实现
+function trim(str) {
+    var i;
+    for (i = 0; i < str.length; i++) {
+        if(str.charAt(i)!=' '&&str.charAt(i)!='    '){
+            break;
+        }
+    }
+    str=str.substring(i,str.length);
+
+    for(i=str.length-1;i>=0;i--)
+    {
+        if(str.charAt(i)!=' '&&str.charAt(i)!=' ')break;
+    }
+    str=str.substring(0,i+1);
+    return str;
+}
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参赛传递
+function each(arr, fn) {
+    if(isArray(arr)){
+        for (var i = 0; i < arr.length; i++) {
+            fn.call(this,arr[i],i);
+        }
+    }
+    else{
+        throw new TypeError()
+    }
+}
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {
+    var count = 0;
+    for(key in obj){
+        count++;
+    }
+    return count;
+}
+// 判断是否为邮箱地址
+function isEmail(emailStr) {
+    return emailStr.test();
+}
+
+// 判断是否为手机号
+function isMobilePhone(phone) {
+    return phone.test()
+}
+
+// 为dom增加一个样式名为newClassName的新样式
+function addClass(element, newClassName) {
+    if(element.classList){
+        element.classList.add(newClassName);
+    }
+    else{
+        if(element.className.indexOf(newClassName)==-1){
+            element.className = element.className + " " + newClassName;
+        }
+    }
+}
