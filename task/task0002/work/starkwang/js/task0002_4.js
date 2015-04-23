acb3fc0e905b5bb25d2c4a4ce27d01ff8faff2f8
starkwang <381152119@qq.com>
2015-04-23 22:35:01 +0800

    complete task0002_4

--- task/task0002/work/starkwang/js/task0002_4.js
+++ task/task0002/work/starkwang/js/task0002_4.js
@@ -1,7 +1,10 @@
-addEvent($("#search"),"keyup",function(){
-	var str = $("#search").value;
-	$("#suggestbox").innerHTML = "";
-	ajax(
+(function(){
+	var nowChoice = -1;
+	function suggest_ajax(){
+		nowChoice = -1;
+		$("#suggestbox").innerHTML = "";
+		var str = $("#search").value;
+		ajax(
 		'http://localhost:1337', 
 		{
 			type:"GET",
@@ -9,22 +12,73 @@ addEvent($("#search"),"keyup",function(){
 				name: str
 			},
 			onsuccess: function (responseText, xhr) {
-        				var suggest_template = "<div class=\"suggest\">{{suggest}}</div>";
+        				var suggest_template = "<div class=\"suggest\" id=\"{{id}}\">{{suggest}}</div>";
         				var arr = responseText.split(",");
         				console.log(arr);
         				arr.length = arr.length - 1;
         				each(arr,function(item,index){
-        					$("#suggestbox").innerHTML = $("#suggestbox").innerHTML + suggest_template.replace(/{{suggest}}/g,item);
+        					$("#suggestbox").innerHTML = $("#suggestbox").innerHTML + suggest_template.replace(/{{suggest}}/g,item).replace(/{{id}}/g,index);
         				})
+        				for(var i = 0 ; i < $(".suggest").length ; i++){
+					addEvent($(".suggest")[i],"click",function(){
+						$("#search").value = this.innerHTML;
+						$("#suggestbox").innerHTML = "";
+						nowChoice = -1;
+					})
+				}
         			}
         		}
-	);
-});
-
-addEvent($("body"),"keyup",function(event){
-	var e = event || window.event;
-	if(e && e.keycode==13){
-	console.log("aa");		
+		);	
 	}
 
-})
+	function chooseDown(){
+		nowChoice++;
+		$("#" + nowChoice.toString()).style.background = "#a0a0a0";
+		$("#" + (nowChoice-1).toString()).style.background = "#ffffff";
+	}
+	function chooseUp(){
+		nowChoice--;
+		$("#" + nowChoice.toString()).style.background = "#a0a0a0";
+		$("#" + (nowChoice+1).toString()).style.background = "#ffffff";	
+	}
+	function choose(){
+		$("#search").value = $("#" + nowChoice.toString()).innerHTML;
+	}
+	suggest = {
+		ajax : suggest_ajax ,
+		chooseDown : chooseDown,
+		chooseUp : chooseUp,
+		choose : choose
+	}
+	window.suggest = suggest;
+})()
+
+addEvent($("#search"),"keyup",function(){
+	e = event || window.event;
+	if( (e.keyCode < 38 || e.keyCode > 40) && e.keyCode != 13){
+		suggest.ajax();
+	}
+	
+	if(e.keyCode==40){
+		suggest.chooseDown();			
+	}
+
+	if(e.keyCode==38){
+		suggest.chooseUp();			
+	}
+
+	if(e.keyCode == 13){
+		suggest.choose();
+		$("#suggestbox").innerHTML = "";
+	}
+});
+
+
+
+// document.onkeydown = function(){
+// 	var e = event || window.event;
+// 	//38 up,    40 down
+// 	if(e.keyCode==38){
+				
+// 	}
+// }
