ba660556bb6a19f8ab7ce4b47d54a75e7f03afc0
dada <417611525@qq.com>
2015-04-23 14:20:09 +0800

    add finish task/task0002/work/fromIRIS/task0002_1.html

--- task/task0002/work/fromIRIS/js/task0002_1.js
+++ task/task0002/work/fromIRIS/js/task0002_1.js
@@ -1,39 +1,79 @@
 $.click("#btn1", getLove);
 function getLove() {
 	var sTr = $("#txt1").value;
-	var arr = sTr.split(",");
-	console.log(arr)
+	var reg = /\uff0c/g;
+	if (reg.test(sTr)) {
+		alert("请输入半角逗号");
+	}else {
+		var arr = sTr.split(",");
+		var uniqArr = uniqArray(arr);
+		var oP = document.createElement("p");
+		$("#div1").appendChild(oP);
+		for (var i = 0; i<uniqArr.length; i++) {
+			if (uniqArr[i].match(/\s+/g)) {
+				uniqArr.splice(i,1);
+				i--;
+			}
+		}
+		oP.innerHTML = uniqArr;
+	}
+}
+
+$.click("#btn2", getLove2);
+function getLove2() {
+	var sTr = $("#txt2").value;
+	var sep = /[,\uff0c\s\u3001;\n\t]+/g
+	var replaceStr = sTr.replace(sep, " ");
+	var arr = replaceStr.split(" ");
 	var uniqArr = uniqArray(arr);
-	console.log(uniqArr)
 	var oP = document.createElement("p");
-	$("#div1").appendChild(oP);
+	$("#div2").appendChild(oP);
 	for (var i = 0; i<uniqArr.length; i++) {
 		if (uniqArr[i].match(/\s+/g)) {
 			uniqArr.splice(i,1);
 			i--;
 		}
 	}
-	for (var j in uniqArr) {
-			oP.innerHTML += uniqArr[j];
-		}
+	oP.innerHTML = uniqArr;
 }
-$.click("#btn2", getLove2);
-function getLove2() {
-	var sTr = $("#txt2").value;
-	var sep = "," || "，" || " " || "　" || "、" || ";";//wrong
-	var arr = sTr.split(" ");
-	console.log(arr)
-	var uniqArr = uniqArray(arr);
-	console.log(uniqArr)
-	var oP = document.createElement("p");
-	$("#div2").appendChild(oP);
-	/*for (var i = 0; i<uniqArr.length; i++) {
-		if (uniqArr[i].match(/\s+/g)) {
-			uniqArr.splice(i,1);
-			i--;
+
+$.click("#btn3", getLove3);
+function getLove3() {
+	var sTr = $("#txt3").value;
+	$("#txt3").value = "";
+	var oTip = document.createElement('p');
+	$("#div3").insertBefore(oTip,$("#btn3"));
+	if (sTr != "") {
+		var sep = /[,\uff0c\s\u3001;\n\t]+/g
+		//先将字符转换成以空格为间的形式
+		var replaceStr = sTr.replace(sep, " ");
+		var arr = replaceStr.split(" ");
+		if (arr.length >= 0 && arr.length <= 10) {
+			var uniqArr = uniqArray(arr);
+			for (var i = 0; i<uniqArr.length; i++) {
+				//将去重得到的空数组去掉
+				if (uniqArr[i].match(/\s+/g)) {
+					uniqArr.splice(i,1);
+					i--;
+				}
+			}
+			//循环生成chenkbox
+			for (var j in uniqArr) {
+				var oLabel = document.createElement("label");
+				oLabel.setAttribute("for", uniqArr[j]);
+				var sCheckName = document.createTextNode(uniqArr[j]);
+				oLabel.appendChild(sCheckName);
+				$("#div3").appendChild(oLabel);
+				var oInput = document.createElement("input");
+				oInput.setAttribute("type", "checkbox");
+				oInput.setAttribute("id", uniqArr[j]);
+				$("#div3").appendChild(oInput);
+			}
+			oP.innerHTML = uniqArr;
+		}else {
+			oTip.innerHTML = "输入小于等于10个兴趣";
 		}
-	}*/
-	for (var j in uniqArr) {
-			oP.innerHTML += uniqArr[j];
-		}
+	}else {
+		oTip.innerHTML = "请输入";
+	}
 }
