78e77af06cbc8504f53d740537e8f0d3ab9dd5d3
dada <417611525@qq.com>
2015-04-24 08:30:12 +0800

    add remove some notes

--- task/task0002/work/fromIRIS/utils.js
+++ task/task0002/work/fromIRIS/utils.js
@@ -13,13 +13,6 @@ function isBool(bool) {
 function isNum(num) {
     return Object.prototype.toString.call(num) === "[object Number]";
 }
-console.log(isArray([]));
-console.log(isFunction(function () {
-    alert('a');
-}));
-/*console.log(isStr([]));
-console.log(isBool(true));
-console.log(isNum(8));*/
 
 //判断数据类型的方法↑
 
@@ -33,27 +26,6 @@ function cloneObject(obj) {
     }
     return result;
 }
-var srcObj = {
-    a: 1,
-    b: {
-        b1: ["hello", "hi"],
-        b2: "JavaScript"
-    },
-    c: ""
-};
-
-var abObj = srcObj;
-var tarObj = cloneObject(srcObj);
-
-srcObj.a = 2;
-srcObj.b.b1[0] = "Hello";
-
-console.log(abObj.a);     //2
-console.log(abObj.b.b1[0]);    //"hello"
-
-console.log(tarObj.a);      // 1
-console.log(tarObj.b.b1[0]);    // "hello"
-
 //递归法深克隆↑
 
 function uniqArray(arr) {
@@ -67,10 +39,6 @@ function uniqArray(arr) {
     }
     return arr;
 }
-var a = [1, 3, 3, 3, 5, 3, 5];
-var b = uniqArray(a);
-console.log(b); // [1, 3, 5, 7]
-
 // 数组去重↑
 
 function trim(str) {
@@ -82,8 +50,6 @@ function trim(str) {
     }
     return toTrim;
 }
-var str = '(hi!  )';
-console.log(trim(str));
 // 字符串去两端空格↑
 
 function each(arr, fn) {
@@ -91,11 +57,6 @@ function each(arr, fn) {
         fn(arr[i], i);
         }
     }　
-var arr = ['java', 'c', 'php', 'html'];
-function output(item, index) {
-    console.log(item + index)
-}
-each(arr, output);  // java, c, php, html
 //实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参赛传递↑
 
 function getObjectLength(obj) {
@@ -105,29 +66,18 @@ function getObjectLength(obj) {
     }
     return i;
 }
-var obj = {
-    a: 1,
-    b: 2,
-    c: {
-        c1: 3,
-        c2: 4
-    }
-};
-console.log(getObjectLength(obj)); // 3
 // 获取一个对象里面第一层元素的数量，返回一个整数↑
 
 function isEmail(emailStr) {
     var reg = /^[0-9a-z_-]+@[a-z0-9]+\.[a-z]{2,4}$/;
     return reg.test(emailStr);
 }
-console.log(isEmail('417611525@qq.com'));
 // 判断是否为邮箱地址↑
 
 function isMobilePhone(phone) {
     var reg = /^[1][0-9]{10}$/g;//第一位是1
     return reg.test(phone);
 }
-console.log(isMobilePhone('15757184650'));
 // 判断是否为手机号↑
 
 function addClass(element, newClassName) {
