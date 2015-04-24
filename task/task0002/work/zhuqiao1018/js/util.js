2a693b5330970e9b05d066cdcfdee1cb7e7a2f3d
zhuqiao1018 <zqno007@qq.com>
2015-04-24 11:40:17 +0800

    save 	new file:   zhuqiao1018/js/test.js 	new file:   zhuqiao1018/js/util.js 	new file:   zhuqiao1018/task0002.html

---
+++ task/task0002/work/zhuqiao1018/js/util.js
@@ -0,0 +1,25 @@
+/* Created by zhuqiao 
+ * on 2015/4/24
+ */
+
+function isArray (arr) {
+    // body...
+    return arr instanceof Array;
+};
+
+function isFunction(fn) {
+
+    return boolean;
+};
+
+function cloneObject(src) {
+
+};
+
+function uniqArray(arr) {
+
+}
+
+function trim(str) {
+
+}
