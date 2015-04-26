343e87d49d3b336e17137326e9b63ba89d6583e9
Aralic <chen917252683@163.com>
2015-04-26 16:11:06 +0800

    space4

--- task/task0002/work/Aralic/js/task0002_1.js
+++ task/task0002/work/Aralic/js/task0002_1.js
@@ -6,15 +6,15 @@ window.onload = function() {
     var  onOff3 = true;
 
     //第一阶段
-	var oInput1 = $('#input1');
-	var oBtn1 = $('#btn1');
+    var oInput1 = $('#input1');
+    var oBtn1 = $('#btn1');
 
-	oBtn1.onclick = function() {
-	    if (oInput1.value != '') {
+    oBtn1.onclick = function() {
+        if (oInput1.value != '') {
 
-	        //对输入框值进行处理，返回的数组去重并且去首位空格
-	        var arr1 = uniqueArray(oInput1.value.split(/,/),true);
-	        
+            //对输入框值进行处理，返回的数组去重并且去首位空格
+            var arr1 = uniqueArray(oInput1.value.split(/,/),true);
+            
             //新创建p标签 添加到父级下面
             if (onOff1) {
                 var oP = document.createElement('p');
@@ -26,18 +26,18 @@ window.onload = function() {
                 //直接渲染创建好的p标签
                  $('#info1').innerHTML = arr1.join(',');
             }
-	        oInput1.value = '';
-	    }
-	};
+            oInput1.value = '';
+        }
+    };
 
     //第二阶段
-	var oInput2 = $('#input2');
-	var oBtn2 = $('#btn2');
-	oBtn2.onclick = function() {
-		var re = /[,\s，、；;\n]/;
-		
-		//对输入框值进行处理，返回的数组去重并且去首位空格
-		var arr = uniqueArray(oInput2.value.split(re),true);
+    var oInput2 = $('#input2');
+    var oBtn2 = $('#btn2');
+    oBtn2.onclick = function() {
+        var re = /[,\s，、；;\n]/;
+        
+        //对输入框值进行处理，返回的数组去重并且去首位空格
+        var arr = uniqueArray(oInput2.value.split(re),true);
 
         //创建p标签 添加到body
         if (onOff2) {   
@@ -49,8 +49,8 @@ window.onload = function() {
         } else {
             $('#info2').innerHTML = arr.join(',');
         }
-		oInput2.value = '';
-	};
+        oInput2.value = '';
+    };
 
 // 用户输入的爱好数量不能超过10个，也不能什么都不输入。
 // 当发生异常时，在按钮上方显示一段红色的错误提示文字，
@@ -65,54 +65,54 @@ window.onload = function() {
     var arr3 = [];
     oInput3.onkeyup = function() {
 
-       	var re = /[,\s，、；;\n]/;
-       	
+        var re = /[,\s，、；;\n]/;
+        
        //对输入框值进行处理，返回的数组去重并且去首位空格
-       	arr3 = oInput3.value.split(re);
-       	for (var i = 0; i<arr3.length; i++) {
+        arr3 = oInput3.value.split(re);
+        for (var i = 0; i<arr3.length; i++) {
             //对得到每项数组元素去首位空格
-       		arr3[i] = trim(arr3[i]);
+            arr3[i] = trim(arr3[i]);
             //用户输入存在重复项
-       		if (!isRepectArray(arr3)) {
-       			tip = 0;
-       			break;
+            if (!isRepectArray(arr3)) {
+                tip = 0;
+                break;
                 //用户输入有空内容项
-       		} else if (arr3[i] == '' && i != arr3.length-1) {
-       			tip = 1;
-       			break;
+            } else if (arr3[i] == '' && i != arr3.length-1) {
+                tip = 1;
+                break;
                 //用户输入超过10个内容项
-       		} else if (arr3.length > 10 && tip != 1) {
-       			tip = 2;
-       			break;
-       		} 
-       	}
+            } else if (arr3.length > 10 && tip != 1) {
+                tip = 2;
+                break;
+            } 
+        }
         //用户全部输入正确情况
-       	if (i == arr3.length) {
-       		tip = 3;
-       	}
+        if (i == arr3.length) {
+            tip = 3;
+        }
 
         //针对用户输入 作出相应提示文字
-       	if (tip != iNow) {
-       		switch (tip) {
-       			case 0:
-       				oInfo.innerHTML = "您输入的爱好重复,请删除重新输入!";
-       				break;
-       			case 1: 
-       				oInfo.innerHTML = '输入为空，请删除重新输入！';
-       				break;
-       			case 2:
-       				oInfo.innerHTML = '您输入超出10个爱好！！';
-       				break;
-       			case 3:
-       				oInfo.innerHTML = '';
-       				break;
-       		}
-       	}
-       	iNow = tip;
+        if (tip != iNow) {
+            switch (tip) {
+                case 0:
+                    oInfo.innerHTML = "您输入的爱好重复,请删除重新输入!";
+                    break;
+                case 1: 
+                    oInfo.innerHTML = '输入为空，请删除重新输入！';
+                    break;
+                case 2:
+                    oInfo.innerHTML = '您输入超出10个爱好！！';
+                    break;
+                case 3:
+                    oInfo.innerHTML = '';
+                    break;
+            }
+        }
+        iNow = tip;
     }
 
     oBtn3.onclick = function() {
-    	if (tip == 3) {
+        if (tip == 3) {
             var str = '';
             for (var i = 0; i<arr3.length; i++) {
                 str += '<label for="#"><input type="checkbox">'+arr3[i]+'</label>';
@@ -127,8 +127,8 @@ window.onload = function() {
             } else {
                 $('#form1').innerHTML = str;
             }
-    		
-    	}
+            
+        }
     }
 
     //判断数组是否有重复项 返回boolean 有重复项 返回false;
