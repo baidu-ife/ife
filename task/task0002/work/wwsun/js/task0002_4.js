a0c2fd6426b9a9a8126d178a3dbfcb13b705f39f
Weiwei SUN <ww.sun@outlook.com>
2015-04-26 13:27:04 +0800

    code refactor

--- task/task0002/work/wwsun/js/task0002_4.js
+++ task/task0002/work/wwsun/js/task0002_4.js
@@ -1,48 +1,50 @@
-/**
- * get input suggestions from server
- * @param input
- * @param url
- */
-function getSuggestions(input, url) {
-    ajax(url, {
-        type: 'GET',
-        data: {name: input},
-        onsuccess: function (data) {
-            // data: ["value1","value2","value3"]
-            var result = JSON.parse(data);
-            each(result, function (item) {
-                displaySuggestions(item, '#suggestions')
-            });
-        },
-        onfail: function (error) {
-            console.log(error);
-        }
+(function() {
+    /**
+     * get input suggestions from server
+     * @param input
+     * @param url
+     */
+    function getSuggestions(input, url) {
+        ajax(url, {
+            type: 'GET',
+            data: {name: input},
+            onsuccess: function (data) {
+                // data: ["value1","value2","value3"]
+                var result = JSON.parse(data);
+                each(result, function (item) {
+                    displaySuggestions(item, '#suggestions')
+                });
+            },
+            onfail: function (error) {
+                console.log(error);
+            }
+        });
+    }
+
+    /**
+     *
+     * @param item
+     * @param targetId '#ID'
+     */
+    function displaySuggestions(item, targetId) {
+        var target = $(targetId);
+        var li = document.createElement('li');
+        li.innerHTML = item;
+        target.appendChild(li);
+    }
+
+
+    $.on('#search-input', 'keyup', function (e) {
+
+        $('#suggestions').innerHTML = '';
+
+        var inputValue = $('#search-input').value;
+        getSuggestions(inputValue, 'http://localhost:8080/api/');
     });
-}
 
-/**
- *
- * @param item
- * @param targetId '#ID'
- */
-function displaySuggestions(item, targetId) {
-    var target = $(targetId);
-    var li = document.createElement('li');
-    li.innerHTML = item;
-    target.appendChild(li);
-}
-
-
-$.on('#search-input', 'keyup', function (e) {
-
-    $('#suggestions').innerHTML = '';
-
-    var inputValue = $('#search-input').value;
-    getSuggestions(inputValue, 'http://localhost:8080/api/');
-});
-
-$.delegate('#suggestions', 'li', 'click', function (e) {
-    var target = e.target || e.srcElement;
-    $('#search-input').value = target.innerText;
-    $('#suggestions').innerHTML = '';
-});
+    $.delegate('#suggestions', 'li', 'click', function (e) {
+        var target = e.target || e.srcElement;
+        $('#search-input').value = target.innerText;
+        $('#suggestions').innerHTML = '';
+    });
+})();
--- task/task0002/work/wwsun/js/task0002_5.js
+++ task/task0002/work/wwsun/js/task0002_5.js
@@ -1,62 +1,64 @@
-var lis = document.querySelectorAll('li');
+(function() {
+    var lis = document.querySelectorAll('li');
 
-[].forEach.call(lis, function(li) {
-    li.addEventListener('dragstart', handleDragStart, false);
-    li.addEventListener('dragenter', handleDragEnter, false);
-    li.addEventListener('dragover', handleDragOver, false);
-    li.addEventListener('dragleave', handleDragLeave, false);
-    li.addEventListener('drop', handleDrop, false);
-    li.addEventListener('dragend', handleDragEnd, false);
-});
-
-
-var dragSrcEl = null;
-
-function handleDragStart(e) {
-    this.style.opacity = '0.4';
-
-    dragSrcEl = this;
-
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-
-function handleDragOver(e) {
-    if (e.preventDefault()) {
-        e.preventDefault(); // allows us to drop
-    }
-
-    e.dataTransfer.dropEffect = 'move';
-    return false;
-}
-
-function handleDragEnter(e) {
-    // this / e.target is the current hover target
-    this.classList.add('over');
-}
-
-function handleDragLeave(e) {
-    // this / e.target is the previous target element
-    this.classList.remove('over');
-}
-
-function handleDrop(e) {
-    if (e.stopPropagation()) {
-        e.stopPropagation(); // stops the browser from redirecting
-    }
-
-    if (dragSrcEl != this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
-        this.parentNode.appendChild(dragSrcEl);
-    }
-
-    return false;
-}
-
-function handleDragEnd(e) {
-    [].forEach.call(lis, function (li) {
-        li.classList.remove('over');
+    [].forEach.call(lis, function(li) {
+        li.addEventListener('dragstart', handleDragStart, false);
+        li.addEventListener('dragenter', handleDragEnter, false);
+        li.addEventListener('dragover', handleDragOver, false);
+        li.addEventListener('dragleave', handleDragLeave, false);
+        li.addEventListener('drop', handleDrop, false);
+        li.addEventListener('dragend', handleDragEnd, false);
     });
-}
+
+
+    var dragSrcEl = null;
+
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';
+
+        dragSrcEl = this;
+
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+
+
+    function handleDragOver(e) {
+        if (e.preventDefault()) {
+            e.preventDefault(); // allows us to drop
+        }
+
+        e.dataTransfer.dropEffect = 'move';
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        // this / e.target is the current hover target
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        // this / e.target is the previous target element
+        this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+        if (e.stopPropagation()) {
+            e.stopPropagation(); // stops the browser from redirecting
+        }
+
+        if (dragSrcEl != this) {
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+            this.parentNode.appendChild(dragSrcEl);
+        }
+
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        [].forEach.call(lis, function (li) {
+            li.classList.remove('over');
+        });
+    }
+})();
