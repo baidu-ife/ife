ee9a764e1b65f5419eb6d06f1c5a5ccffdc571cd
frozenwings <frozenwings@yeah.net>
2015-04-27 18:50:07 +0800

    fix bug of task0002_2

--- task/task0002/work/frozenwings/js/task0002_2.js
+++ task/task0002/work/frozenwings/js/task0002_2.js
@@ -17,6 +17,8 @@ function computeTime(){
     var interval= target.time - (new Date());
     if(interval <= 0){
       clearInterval(timer);
+        $('.timer .interval').innerHTML = '0年0月0日0小时0分0秒';
+      return;
     }
     var interval = Math.floor(interval/1000); //时间间隔（秒）    
     var year = Math.floor(interval/60/60/24/365);    
@@ -38,19 +40,24 @@ function computeTime(){
     second = addPrefixZero(second);
 
     $('.timer .interval').innerHTML = year + '年'+month+'月'+date+'日'+hour+'小时'+minute+'分'+second+'秒';
-    //console.log(year + ',' + month + ',' + date + ',' + hour + ',' + minute + ',' + second);
 }
 function startTimer(){
   computeTime();
   timer = setInterval(computeTime,1000);  
 }
-
+function showTips(){
+    $('.tips').innerHTML = '日期格式必须为yyyy-mm-dd,并且大于当前日期';
+}
+function clearTips(){
+    $('.tips').innerHTML = '';
+}
 $.on($('.timer button'),'click',function(e){
   if(timerStart && timer > 0){
     clearInterval(timer);
     $('.timer button').innerHTML = 'Start';
     timerStart = false;
-  }else{    
+  }else{
+    clearTips();
     var inputDate = $('.timer input').value;
     //YYYY-MM-DD -> mm/dd/yyyy
     var arr = inputDate.split('-');
@@ -58,10 +65,13 @@ $.on($('.timer button'),'click',function(e){
     target.month = arr[1];
     target.date = arr[2];
     target.time = new Date([target.month,target.date,target.year].join('/'));
-
-    $('.timer .target').innerHTML = '距离'+target.year+'年'+target.month+'月'+target.date+'日还有';
-    startTimer(target);  
-    $('.timer button').innerHTML = 'Stop';
-    timerStart = true;
+    if(typeof target.time === 'undefined' || target.time.toString() === 'Invalid Date' || +target.time < +(new Date())){
+        showTips();
+    }else{
+        $('.timer .target').innerHTML = '距离'+target.year+'年'+target.month+'月'+target.date+'日还有';
+        startTimer(target);
+        $('.timer button').innerHTML = 'Stop';
+        timerStart = true;
+    }
   }
 })
