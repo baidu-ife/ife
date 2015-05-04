c1b0856fb1e7725e67aad81dcb60694fa41f775d
Daphne Chang <Daphne.Chang@foxmail.com>
2015-05-04 10:49:03 +0800

    Clear useless files

---
+++ task/task0002/work/DaphneChang/xxxx.js
@@ -0,0 +1,24 @@
+function $(id) {
+    return document.getElementById(id);
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function renderResult(result) {
+    $("result").innerHTML = result;
+}
+
+function addEventHandle() {
+    var num1 = $("number1").value;
+    var num2 = $("number2").value;
+    var result = add(num1, num2);
+    renderResult(result);
+}
+
+function initEvent() {
+    $("addbtn").addEventListener("click", addEventHandle, false);
+}
+
+initEvent();
