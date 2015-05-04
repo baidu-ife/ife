a25ac90e8cad9ac23dfa150a97c291ddd5c49066
xionCode <1070413503@qq.com>
2015-05-04 20:01:26 +0800

    修复task0002_1出现的问题

--- task/task0002/js/task0002_1.js
+++ task/task0002/js/task0002_1.js
@@ -7,13 +7,16 @@ var fun = function() {
 	//init结束
 	var hobbyList = uniqArray($(".input").value.split(/[,\s;\/]/));
 	console.log(hobbyList);
+	console.log(":"+hobbyList.length);
 	for(var i = 0, len = hobbyList.length;i < len;i++) {
 		if(hobbyList[i] == "") {
-			hobbyList.splice(i,1);
+			console.log(123);
+			console.log(hobbyList.splice(i,1));
 		}
 	}
+	// hobbyList的length已经改变，需要重新赋值
+	len = hobbyList.length;
 	if(len <= 10 && len > 0) {
-		
 		for(var i = 0;i < len;i++) {
 			var choice = document.createElement("input");
 			choice.type = "checkbox";
