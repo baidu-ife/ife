a3ff5e1d820d1158e9fa6f6bdb3dd53379be54d9
Weiwei SUN <ww.sun@outlook.com>
2015-05-08 13:48:40 +0800

    use localstorage to keep the categories and tasks

--- task/task0003/work/wwsun/scripts/task0003.js
+++ task/task0003/work/wwsun/scripts/task0003.js
@@ -1,40 +1,63 @@
-// todo: 使用 localStorage 保存当前操作的分类和任务
 // todo: 禁止添加重复的分类名
-
-// init: mock data
-var categories = [];
-
-var tasks = [];
+// todo: 修改任务内容
+// todo: 允许删除分类（鼠标hover显示删除按钮，需要confirm）
+// todo: 允许更改任务状态
 
 var storage = getLocalStorage(); // for compatibility
+
+function getCategoryArray() {
+    var categoryArray = storage.getItem('categoryArray');
+    if (!categoryArray) {
+        categoryArray = [];
+        storage.setItem('categoryArray', JSON.stringify(categoryArray));
+    } else {
+        categoryArray = JSON.parse(categoryArray);
+    }
+    return categoryArray;
+}
+
+function getTaskArray() {
+    var taskArray = storage.getItem('taskArray');
+    if (!taskArray) {
+        taskArray = [];
+        storage.setItem('taskArray', JSON.stringify(taskArray));
+    } else {
+        taskArray = JSON.parse(taskArray);
+    }
+    return taskArray;
+}
+
+var categories = getCategoryArray();
+var tasks = getTaskArray();
 
 // now only support two level category
 function buildCategoryMenu() {
     var i,
         n;
     var list = $('#categoryList');
+    var categories = getCategoryArray();
 
     list.innerHTML = '';
 
-    for (i=0, n=categories.length; i<n; i++) {
+    for (i = 0, n = categories.length; i < n; i++) {
 
-        if(categories[i].parent==="") {
+        if (categories[i].parent === "") {
             var li = document.createElement('li');
             var a = document.createElement('a');
-            a.setAttribute('href','#');
+            a.setAttribute('href', '#');
             a.setAttribute('data-index', categories[i].id);
             a.innerHTML = categories[i].name;
             li.appendChild(a);
 
             // add secondary categories
-            if(categories[i].subCategories.length > 0) {
+            if (categories[i].subCategories.length > 0) {
                 var subul = document.createElement('ul');
                 var j,
                     m;
-                for (j=0, m=categories[i].subCategories.length; j<m; j++) {
+                for (j = 0, m = categories[i].subCategories.length; j < m; j++) {
                     var subli = document.createElement('li');
                     var suba = document.createElement('a');
-                    suba.setAttribute('href','#');
+                    suba.setAttribute('href', '#');
 
                     var subCategory = getCategoryById(parseInt(categories[i].subCategories[j]));
                     suba.setAttribute('data-index', subCategory.id);
@@ -57,16 +80,17 @@ function setupCategoryDropdownMenu() {
         n;
 
     var select = $('#parentInput');
+    var categories = getCategoryArray();
 
     select.innerHTML = '';
 
     var defaultOption = document.createElement('option');
-    defaultOption.setAttribute('value',"");
+    defaultOption.setAttribute('value', "");
     defaultOption.innerHTML = '无父类';
     select.appendChild(defaultOption);
 
-    for (i=0, n=categories.length; i<n; i++) {
-        if(categories[i].parent==="") {
+    for (i = 0, n = categories.length; i < n; i++) {
+        if (categories[i].parent === "") {
             var option = document.createElement('option');
             option.setAttribute('value', categories[i].id);
             option.innerHTML = categories[i].name;
@@ -77,44 +101,111 @@ function setupCategoryDropdownMenu() {
 }
 
 function getCategoryById(id) {
+    var categories = getCategoryArray();
+
     var i,
         n;
 
-    for (i=0, n=categories.length; i<n; i++) {
-        if(categories[i].id === id) {
-            return categories[i];
+    for (i = 0, n = categories.length; i < n; i++) {
+        if (categories[i].id === id) {
+
+            if (categories[i] instanceof Category) {
+                return categories[i];
+            } else {
+                var cat = new Category(categories[i].name, categories[i].parent);
+                cat.id = categories[i].id;
+                cat.subCategories = categories[i].subCategories;
+                cat.tasks = categories[i].tasks;
+                return cat;
+            }
         }
     }
 }
 
 function getTaskById(id) {
+    var tasks = getTaskArray();
+
     var i,
         n;
 
-    for (i=0, n=tasks.length; i<n; i++) {
-        if(tasks[i].id === id) {
-            return tasks[i];
+    for (i = 0, n = tasks.length; i < n; i++) {
+        if (tasks[i].id === id) {
+
+            if (tasks[i] instanceof Task) {
+                return tasks[i];
+            } else {
+                var tsk = new Task(tasks[i].title, tasks[i].category, tasks[i].date, tasks[i].content);
+                tsk.id = tasks[i].id;
+                tsk.status = tasks[i].status;
+                return tsk;
+            }
         }
     }
 }
 
 function addNewCategory(name, parentId) {
+    var categoryArray = getCategoryArray();
+
+    var superCategory;
     var category = new Category(name, parentId, 0);
+
     if (parentId != '') {
 
         // find super category
-        var superCategory = getCategoryById(parseInt(parentId));
+        superCategory = getCategoryById(parseInt(parentId));
         superCategory.addSubCategory(category.id);
 
+        // update the parent category item
+        storage.setItem(superCategory.id, JSON.stringify(superCategory));
+
+        var i,
+            n;
+
+        for (i = 0, n = categoryArray.length; i < n; i++) {
+            if (categoryArray[i].id === superCategory.id) {
+                categoryArray[i].subCategories = superCategory.subCategories.slice(); // Array Copy
+            }
+        }
     }
-    categories.push(category);
+
+    storage.setItem(category.id, JSON.stringify(category));
+    categoryArray.push(category);
+
+    storage.setItem('categoryArray', JSON.stringify(categoryArray));
 }
 
+/**
+ * add new task to the specified category
+ * @param title
+ * @param categoryId
+ * @param date
+ * @param content
+ * @returns {Task}
+ */
 function addNewTask(title, categoryId, date, content) {
+
+    var taskArray = getTaskArray();
+    var categoryArray = getCategoryArray();
+
     var task = new Task(title, categoryId, date, content);
     var category = getCategoryById(parseInt(categoryId));
+
     category.addTask(task.id);
-    tasks.push(task);
+
+    var i,
+        n;
+
+    for (i = 0, n = categoryArray.length; i < n; i++) {
+        if (categoryArray[i].id === category.id) {
+            categoryArray[i].tasks = category.tasks.slice(); // Array copy
+        }
+    }
+
+    storage.setItem(task.id, JSON.stringify(task)); // could be delete
+    taskArray.push(task);
+
+    storage.setItem('taskArray', JSON.stringify(taskArray));
+    storage.setItem('categoryArray', JSON.stringify(categoryArray));
 
     buildTaskListByCategory(category); // refresh the task list
 
@@ -129,20 +220,19 @@ function buildTaskListByCategory(category) {
     taskList.innerHTML = ''; // empty the task list
 
     if (category.tasks !== undefined) {
-        for (i=0, n=category.tasks.length; i<n; i++) {
+        for (i = 0, n = category.tasks.length; i < n; i++) {
             var li = document.createElement('li');
             var a = document.createElement('a');
-            a.setAttribute('href','#');
+            a.setAttribute('href', '#');
 
             var task = getTaskById(category.tasks[i]);
 
-            a.setAttribute('data-index',task.id);
+            a.setAttribute('data-index', task.id);
             a.innerHTML = task.title;
             li.appendChild(a);
             taskList.appendChild(li);
         }
     }
-
 }
 
 function displayTaskDetail(task) {
@@ -153,25 +243,23 @@ function displayTaskDetail(task) {
 
 function setupTaskListDelegate(selector) {
     var taskUl = $(selector);
-    EventUtil.addHandler(taskUl, 'click', function(event) {
+    EventUtil.addHandler(taskUl, 'click', function (event) {
         event = EventUtil.getEvent(event);
         var target = EventUtil.getTarget(event);
-
         var task = getTaskById(parseInt(target.dataset.index));
-
         displayTaskDetail(task);
     });
 }
 
 function setupMenuDelegate(selector) {
     var list = $(selector);
-    EventUtil.addHandler(list, 'click', function(event) {
+    EventUtil.addHandler(list, 'click', function (event) {
         event = EventUtil.getEvent(event);
         var target = EventUtil.getTarget(event);
 
         if (target.dataset.index === undefined) { // get attribute 'data-index'
             // do nothing
-        } else if(target.dataset.index == 'all-tasks') {
+        } else if (target.dataset.index == 'all-tasks') {
             // todo: buildAllTasks();
         } else {
             storage.setItem('currentCategoryId', target.dataset.index);
@@ -192,7 +280,7 @@ function appInit() {
 
 // event binding
 
-$.click('#newCategoryBtn', function() {
+$.click('#newCategoryBtn', function () {
     var form = $('#newCategoryForm');
     var name = form.elements['name'].value;
 
@@ -205,7 +293,7 @@ $.click('#newCategoryBtn', function() {
     setupCategoryDropdownMenu(); // refresh category select items
 });
 
-$.click('#addTaskBtn', function() {
+$.click('#addTaskBtn', function () {
     var form = $('#newTaskForm');
     var title = form.elements['title'].value;
     var due = form.elements['due'].value;
@@ -231,7 +319,7 @@ buildCategoryMenu();
 
 function print() {
     var i, n;
-    for (i=0, n=categories.length; i<categories.length; i++) {
+    for (i = 0, n = categories.length; i < categories.length; i++) {
         console.log(categories[i]);
     }
 }
