10283d0b2bc2bcab4c0bfa90648b3b9e82ee289c
withershins <609541500@qq,com>
2015-07-15 13:31:30 +0800

    modify

--- task/task0002/util.js
+++ task/task0002/util.js
@@ -30,29 +30,6 @@ function cloneObject(src) {
   return result;
 }
 
-// 方法二
-//instanceof运算符用来比较两个操作数的构造函数
-function cloneObject(Obj) {
-    var buf;
-    if (Obj instanceof Array) {
-        buf = [];  //创建一个空的数组
-        var i = Obj.length;
-        while (i--) {
-            buf[i] = cloneObject(Obj[i]);
-        }
-        return buf;
-    }else if (Obj instanceof Object){
-        buf = {};  //创建一个空对象
-        for (var k in Obj) {  //为这个对象添加新的属性
-            buf[k] = cloneObject(Obj[k]);
-        }
-        return buf;
-    }else{
-        return Obj;
-    }
-}
-
-
 // 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
 function uniqArray(arr) {
     var arrNew = [];
